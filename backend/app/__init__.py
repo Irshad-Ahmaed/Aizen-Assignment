cfd2badce8c09658816d39c826f53ed0f4f66d25
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2025-03-14 20:10:48 +0530

    Cors fix

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -10,7 +10,7 @@ from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, resources={r"/api/v2/*": {"origins": "http://localhost:5173"}})  # Allow only specific origin
+    CORS(app, resources={r"/api/v2/*": {"origins": ["http://localhost:5173", "https://aizen-assignment.vercel.app"]}})  # Allow only specific origin
     app.config.from_object(Config)
 
     # Initialize extensions
